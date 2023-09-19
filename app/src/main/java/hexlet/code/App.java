372046833452a5ffc8acdc08b4ef9ad967c74ff5
Viacheslav Oblog <slavaoblog@ya.ru>
2023-09-19 15:44:13 +0300

    step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,24 +1,56 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
 public class App {
+    private static final String JDBC_URL_H2 = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1";
+
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
+    }
+
+    private static String getJdbcUrlFromEnv() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", JDBC_URL_H2);
     }
 
     private static void addRoutes(Javalin app) {
         app.get("/", ctx -> ctx.result("Hello World"));
     }
 
-    public static Javalin getApp() {
+    public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getJdbcUrlFromEnv());
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var url = App.class.getClassLoader().getResource("schema.sql");
+        assert url != null;
+        var file = new File(url.getFile());
+        var sql = Files.lines(file.toPath())
+                .collect(Collectors.joining("\n"));
+
+        try (var connection = dataSource.getConnection();
+        var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
 
         addRoutes(app);
 
+        //для чего это?
         app.before(ctx -> {
             ctx.attribute("ctx", ctx);
         });
@@ -26,7 +58,7 @@ public class App {
     return app;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException {
         Javalin app = getApp();
         app.start(getPort());
     }
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,9 @@
+package hexlet.code.model;
+
+import java.sql.Timestamp;
+
+public class Url {
+    private long id;
+    private String name;
+    private Timestamp createdAt;
+}
