4be21133705bd1f78deb1c816a6b56bdb9ae849f
Viacheslav Oblog <slavaoblog@ya.ru>
2023-09-21 23:45:32 +0300

    step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,11 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import io.javalin.rendering.template.JavalinThymeleaf;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,7 +20,7 @@ public class App {
     private static final String JDBC_URL_H2 = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1";
 
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault("PORT", "8080");
         return Integer.valueOf(port);
     }
 
@@ -23,8 +28,25 @@ public class App {
         return System.getenv().getOrDefault("JDBC_DATABASE_URL", JDBC_URL_H2);
     }
 
+    private static boolean isDevelopment() {
+        return getJdbcUrlFromEnv().equals(JDBC_URL_H2);
+    }
+
     private static void addRoutes(Javalin app) {
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.render("startPage.html"));
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
     }
 
     public static Javalin getApp() throws IOException, SQLException {
@@ -44,8 +66,12 @@ public class App {
         }
         BaseRepository.dataSource = dataSource;
 
-        var app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
+        Javalin app = Javalin.create(config -> {
+            if (isDevelopment()) {
+                config.plugins.enableDevLogging();
+            }
+            config.staticFiles.enableWebjars();
+            JavalinThymeleaf.init(getTemplateEngine());
         });
 
         addRoutes(app);
